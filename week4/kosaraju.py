13342d29b742d5dfa8cce38b46152ca2e5ad03b0
Felipe Pedrosa <felipe.pedrosa@gmail.com>
2016-10-28 09:27:18 -0400

    update kosaraju.py
    the change in lines 32-34 made the difference between the code running in several hours to seconds

--- week4/kosaraju.py
+++ week4/kosaraju.py
@@ -7,17 +7,15 @@ Created on Sat Oct  8 01:17:06 2016
 import sys
 
 #open the file passed as argument to the call
-f = open(sys.argv[1], 'r')
+f = open("SCC.txt")
 
 def first_pass(graph):
     ordering = []
+    ordering_set = set()
     explored = set()
     
     print "Started first pass..."  
-    total = len(graph.keys())
-    num_cur = 0
-    progress = 0
-    
+  
     for vertex in range(max(graph.keys()), 0, -1):
         if vertex not in explored:
             stack = [vertex]
@@ -25,24 +23,22 @@ def first_pass(graph):
                 u = stack.pop()
                 if u not in explored:
                     explored.add(u)
-                    num_cur += 1
-                    aux = progress
-                    progress = int(float(num_cur)/total *100)
-                    if progress - aux >= 1:
-                        print "Progress = " + str(progress)
-                    stack.append(u)
+                    stack.append(u)  #append it back for backtracking purpose
                     if u in graph:
                         for v in graph[u]:
                             if v not in explored:
                                 stack.append(v)
                 else:
-                    if u not in ordering:
+                    if u not in ordering_set:
                         ordering.append(u)
+                        ordering_set.add(u)
     return ordering
 
 def second_pass(graph, ordering):
     explored = set()
     leader = {}
+    
+    print "Started second pass..." 
     
     for u in reversed(ordering):
         if u not in explored:
@@ -73,21 +69,17 @@ for line in f:
         
 #convert list of edges into graph adjacency list format
 G = {}
+G_rev = {}
 for arc in arcs:
     # if the first node of arc isn't a key in G, add it
     if arc[0] not in G:
         G[arc[0]] = [arc[1]] #create entry in dict
     else:
         G[arc[0]].append(arc[1]) #append to existing entry
-
-#create adjacency list with inverted arcs
-G_rev = {}
-for arc in arcs:
     if arc[1] not in G_rev:
         G_rev[arc[1]] = [arc[0]]
     else:
         G_rev[arc[1]].append(arc[0])
-
 print "Finished reading G and G_rev"
 
 SCC = second_pass(G, first_pass(G_rev))
@@ -95,5 +87,3 @@ SCC = second_pass(G, first_pass(G_rev))
 size_scc = [len(SCC[i]) for i in SCC]
 solution = sorted(size_scc, reverse=True)
 print solution[0:5]
-
-
